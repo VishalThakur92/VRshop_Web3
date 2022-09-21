734d767f9849c82865b7fc8a0e3dc81eca2cc6af
Vish <vishal-145@live.com>
2022-09-21 14:31:23 +0530

    add music

---
+++ VRshop_Web3/Assets/Scripts/Core/Managers/Sounds/SoundManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
